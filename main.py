7c48857208a373b512f8379760a81a4db640beec
Alexandre <alexandre.gerard.dev@gmail.com>
2024-08-05 13:18:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+import os
+import random
+
 import chess
 import chess.engine
-import random
-import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/evaluate', methods=['POST'])
+
+@app.route("/evaluate", methods=["POST"])
 def evaluate():
-    fen = request.json['fen']
-    level = request.json['level']
+    fen = request.json["fen"]
+    level = request.json["level"]
     board = chess.Board(fen)
 
     # Assuming stockfish-ubuntu-x86-64-avx2 is in the same directory as this script
-    stockfish_path = os.path.join(os.path.dirname(__file__), 'stockfish-ubuntu-x86-64-avx2')
+    stockfish_path = os.path.join(
+        os.path.dirname(__file__), "stockfish-ubuntu-x86-64-avx2"
+    )
 
     with chess.engine.SimpleEngine.popen_uci(stockfish_path) as engine:
         result = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=5)
-        moves_scores = [(entry['pv'][0].uci(), entry['score'].white().score(mate_score=10000)) for entry in result]
+        moves_scores = [
+            (entry["pv"][0].uci(), entry["score"].white().score(mate_score=10000))
+            for entry in result
+        ]
 
         if level == 1:
-            chosen_move = random.choice(moves_scores[:5])[0]  # Chooses randomly from the top 5 moves
+            chosen_move = random.choice(moves_scores[:5])[
+                0
+            ]  # Chooses randomly from the top 5 moves
         elif level == 2:
-            chosen_move = random.choice(moves_scores[:3])[0]  # Chooses randomly from the top 3 moves
+            chosen_move = random.choice(moves_scores[:3])[
+                0
+            ]  # Chooses randomly from the top 3 moves
         else:  # level == 3 or any other value
             chosen_move = moves_scores[0][0]  # Always chooses the best move
 
         evaluation = moves_scores[0][1]  # Evaluation of the best move
 
-    return jsonify({'evaluation': evaluation, 'move_to_play': chosen_move, 'best_move':  moves_scores[0][0]})
+    return jsonify(
+        {
+            "evaluation": evaluation,
+            "move_to_play": chosen_move,
+            "best_move": moves_scores[0][0],
+        }
+    )
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port, debug=True)
